a3b425113b975b8b4a6bd2ff80bb60bf485b1e2c
nate <n4t311@gmail.com>
2023-05-18 14:40:26 +0900

    adds nft data

--- src/demos/recommender/components/RecommenderNFTCard.js
+++ src/demos/recommender/components/RecommenderNFTCard.js
@@ -78,15 +78,10 @@ export const RecommenderNFTCard = ({ nft, nftClickHandler }) => {
                   height={200}
                 />
                 <Stack mt="6" spacing="3">
-                  <Heading size="md">Living room Sofa</Heading>
-                  <Text>
-                    This sofa is perfect for modern tropical spaces, baroque
-                    inspired spaces, earthy toned spaces and for people who love
-                    a chic design with a sprinkle of vintage design.
+                <Text mb="2">
+                    {collectionName != "" ? collectionName : nft?.address}
                   </Text>
-                  <Text color="blue.600" fontSize="2xl">
-                    $450
-                  </Text>
+                  <Text>{nft?.score?.toFixed(2)}</Text>
                 </Stack>
               </CardBody>
             </Card>

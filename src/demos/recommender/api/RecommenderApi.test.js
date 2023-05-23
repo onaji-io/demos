59ae967a27780b0786d8db7d980d7a4989348f39
nate <n4t311@gmail.com>
2023-05-23 17:37:19 +0900

    adds recommender test

---
+++ src/demos/recommender/api/RecommenderApi.test.js
@@ -0,0 +1,44 @@
+import { getRecommendationsFromOnaji } from "./RecommenderApi";
+
+global.fetch = jest.fn();
+
+beforeEach(() => {
+  fetch.mockClear();
+});
+
+test("getRecommendationsFromOnaji fetches data from the api", async () => {
+  const wallet = "0xabc123";
+  const apiResponse = {
+    contract_addresses: ["0x123456", "0x789101"],
+    scores: [4.117968559265137, 4.074355125427246],
+    runtime_milliseconds: 187.837890625,
+    wallet_contents: ["0xdef456"],
+  };
+
+  fetch.mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(apiResponse),
+    })
+  );
+
+  const data = await getRecommendationsFromOnaji(wallet);
+
+  expect(fetch).toHaveBeenCalledWith(
+    `https://staging-api.onaji.io/v1/recommend/contracts?blockchain=ETH&wallet_address=${wallet}&k=10&recently_popular_weight=0&return_wallet_content=1&exclude_owned_contracts=1`,
+    { credentials: "include" }
+  );
+
+  expect(data).toEqual(apiResponse);
+});
+
+test("getRecommendationsFromOnaji throws an error if the api call fails", async () => {
+  const wallet = "0xabc123";
+  // simulate an api error
+  fetch.mockImplementation(() => Promise.reject("API error"));
+
+  try {
+    const res = await getRecommendationsFromOnaji(wallet);
+  } catch (err) {
+    expect(err).toBe("API error");
+  }
+});

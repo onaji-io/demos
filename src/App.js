78b45b91ce8a2eac92a676da9f3ecb0142f5f422
nate <n4t311@gmail.com>
2023-05-25 23:47:14 +0900

    cleanup and wildcard redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,13 +6,7 @@ import {
   Navigate,
   Routes,
 } from "react-router-dom";
-import {
-  Button,
-  ChakraProvider,
-  Container,
-  extendTheme,
-  Input,
-} from "@chakra-ui/react";
+import { ChakraProvider, Container, extendTheme } from "@chakra-ui/react";
 import Recommender from "./demos/recommender/Recommender";
 import VisualSearch from "./demos/visual-search/VisualSearch";
 import TraitSearch from "./demos/trait-search/TraitSearch";
@@ -20,34 +14,6 @@ import TraitSearch from "./demos/trait-search/TraitSearch";
 export const CDN_URL_BASE_PATH = "https://cdn.onaji.io/";
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAuthorized, setIsAuthorized] = useState(false);
-  // TODO: Figure out a more convenient way to do auth
-  const login = async (username, password) => {
-    const data = new URLSearchParams();
-    data.append("username", username);
-    data.append("password", password);
-    return fetch(`https://demo-api.onaji.io/v1/auth/login`, {
-      credentials: "include",
-      method: "post",
-      body: data,
-    });
-  };
-  const loginSubmitHandler = async () => {
-    if (!username || !password) {
-      return;
-    }
-    const res = await login(username, password);
-
-    if (res.status !== 204) {
-      setIsAuthorized(false);
-    } else {
-      console.log("success");
-      setIsAuthorized(true);
-    }
-  };
-
   const theme = extendTheme({
     styles: {
       global: {
@@ -88,6 +54,10 @@ function App() {
               <Route path="/trait-search" element={<TraitSearch />} />
               {/* Automatically route to the recommender demo for now */}
               <Route path="/" element={<Navigate to="/recommender" />} />
+              <Route
+                path="*"
+                element={<Navigate to="/recommender" replace />}
+              />
             </Routes>
           </div>
         </Router>

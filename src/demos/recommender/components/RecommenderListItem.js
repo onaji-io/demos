0defa7293953327f5a897eb994956cd2da1e09d8
nate <n4t311@gmail.com>
2023-05-26 10:27:53 +0900

    fallback img for wallet contents

--- src/demos/recommender/components/RecommenderListItem.js
+++ src/demos/recommender/components/RecommenderListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Flex, Image, Text } from "@chakra-ui/react";
+import FallbackImage from "./FallbackImage.png";
 
 export const RecommenderListItem = ({ collection, onItemClick }) => {
   if (collection == null || !collection?.name || !collection?.address) {
@@ -21,6 +22,7 @@ export const RecommenderListItem = ({ collection, onItemClick }) => {
         marginRight={4}
         borderRadius={4}
         alt="nft image"
+        fallbackSrc={FallbackImage}
       />
       <Text>{collection?.name ?? collection?.address}</Text>
     </Flex>

89462bec044f1822a93279a1184711725cbac24c
nate <n4t311@gmail.com>
2023-06-02 23:30:08 +0900

    border radius

--- src/demos/shared/NFTCard.js
+++ src/demos/shared/NFTCard.js
@@ -1,25 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {
-  Box,
-  Button,
-  Checkbox,
-  CheckboxGroup,
-  Divider,
-  Fade,
-  Flex,
-  Heading,
   Image,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-  Select,
-  Stack,
-  Text,
 } from "@chakra-ui/react";
 
 export const NFTCard = ({ nft }) => {
   return (
-    <Image src={nft.image} width={250} height={250} marginBottom={8}></Image>
+    <Image src={nft.image} width={250} height={250} marginBottom={8} borderRadius={'15px'}></Image>
   );
 };
